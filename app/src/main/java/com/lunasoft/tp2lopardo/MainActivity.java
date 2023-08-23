ebe54c7762efdc3ea4c56de9d76c1e31cd9e20f2
Luna Lopardo <luna.lopardo@gmail.com>
2023-08-23 20:02:34 -0300

    Cambios

--- app/src/main/java/com/lunasoft/tp2lopardo/MainActivity.java
+++ app/src/main/java/com/lunasoft/tp2lopardo/MainActivity.java
@@ -1,14 +1,14 @@
+// MainActivity
 package com.lunasoft.tp2lopardo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.Spanned;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.CompoundButton;
 
 import com.lunasoft.tp2lopardo.databinding.ActivityMainBinding;
 
@@ -23,71 +23,56 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        vm = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+        vm = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
-        /*
-            Lógica para elegir el edit text correcto según qué radio button sea elegido.
-         */
+        // Envía información de los números ingresados y el estado de los radio buttons
         binding.btConvertir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double amount;
-                if (binding.rbDolares.isChecked()) {
-                    amount = Double.parseDouble(binding.etDolares.getText().toString());
-                } else {
-                    amount = Double.parseDouble(binding.etEuros.getText().toString());
-                }
-                vm.convertir(amount, binding.rbDolares.isChecked());
+                vm.convertir(
+                        binding.etDolares.getText().toString(),
+                        binding.etEuros.getText().toString(),
+                        binding.rbDolares.isChecked());
             }
         });
 
-        /*
-            Manejo de radio buttons:
-            - Si el RB de dolar esta tildado, se destilda el de euro y se deshabilita el edit text
-            para ingresar euros. De la misma forma, si el RB de euro está tildado, se deshabilita
-            el ET de dolar.
-            - También se limpian los campos que tienen datos (edit texts y el de resultado) cuando
-            se elige un RB diferente)
-         */
-        binding.rbDolares.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                binding.etDolares.setEnabled(true);
-                binding.etEuros.setEnabled(false);
-                binding.rbEuros.setChecked(false);
-                binding.etEuros.setText("");
-                binding.tvResultado.setText("");
-            }
-        });
-
-        binding.rbEuros.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                binding.etDolares.setEnabled(false);
-                binding.etEuros.setEnabled(true);
-                binding.rbDolares.setChecked(false);
-                binding.etDolares.setText("");
-                binding.tvResultado.setText("");
-            }
-        });
-
-        // Busqué un filtro para no permitir ingresar letras en los ET.
-        InputFilter numericInputFilter = new InputFilter() {
+        // Observa el cambio en el estado del RB rbDolares
+        binding.rbDolares.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public CharSequence filter(CharSequence source, int start, int end,
-                                       Spanned dest, int dstart, int dend) {
-                for (int i = start; i < end; i++) {
-                    if (!Character.isDigit(source.charAt(i))) {
-                        return "";
-                    }
-                }
-                return null;
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                vm.setDolaresChecked(isChecked);
             }
-        };
+        });
 
-        binding.etDolares.setFilters(new InputFilter[]{numericInputFilter});
-        binding.etEuros.setFilters(new InputFilter[]{numericInputFilter});
+        // Observa el cambio en el estado del RadioButton rbEuros
+        binding.rbEuros.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                vm.setEurosChecked(isChecked);
+            }
+        });
+
+        // Observa el cambio en el estado de los ET desde el viewmodel, si devuelve verdadero,
+        // activa el campo para editar y elimina los campos para que no queden números viejos
+        vm.getEurosET().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isEnabled) {
+                binding.etEuros.setEnabled(isEnabled);
+                binding.etDolares.setText("");
+            }
+        });
+
+        vm.getDolaresET().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isEnabled) {
+                binding.etDolares.setEnabled(isEnabled);
+                binding.etEuros.setText("");
+            }
+        });
 
         vm.getResultLiveData().observe(this, result -> {
             binding.tvResultado.setText(result);
         });
+
     }
 }
--- app/src/main/java/com/lunasoft/tp2lopardo/MainActivityViewModel.java
+++ app/src/main/java/com/lunasoft/tp2lopardo/MainActivityViewModel.java
@@ -2,6 +2,7 @@ package com.lunasoft.tp2lopardo;
 
 import android.app.Application;
 import android.content.Context;
+import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -13,7 +14,9 @@ import java.text.DecimalFormat;
 public class MainActivityViewModel extends AndroidViewModel {
 
     private Context context;
-    private MutableLiveData<String> resultLiveData ;
+    private MutableLiveData<String> resultLiveData;
+    private MutableLiveData<Boolean> eurosET;
+    private MutableLiveData<Boolean> dolaresET;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
@@ -27,26 +30,52 @@ public class MainActivityViewModel extends AndroidViewModel {
         }
         return resultLiveData;
     }
+    public LiveData<Boolean> getEurosET() {
+        if (eurosET == null) {
+            eurosET= new MutableLiveData<Boolean>();
+        }
+        return eurosET;
+    }
+    public LiveData<Boolean> getDolaresET() {
+        if (dolaresET == null) {
+            dolaresET= new MutableLiveData<Boolean>();
+        }
+        return dolaresET;
+    }
 
     //Para redondear lo más específicamente posible
     private final DecimalFormat df = new DecimalFormat("0.00");
 
-    /*
-    Saqué un valor de conversión de internet. El método recibe un boolean "esDolar" si está
-    el radio button de Dolar a Euro checkeado. Dependiendo si es dolar o euro, divide o multiplica
-    por el valor de conversión seteado.
-     */
-    public void convertir(Double cantidad, boolean esDolar) {
+    /* El método recibe un boolean "esDolar" si está el radio button de Dolar a Euro checkeado.
+    Dependiendo si es dolar o euro, divide o multiplica por el valor de conversión seteado. */
+    public void convertir(String dol, String eur, boolean esDolar) {
         double conversion = 0.92;
-        double cantidadFinal = esDolar ? cantidad * conversion : cantidad / conversion;
-        double cf = Double.parseDouble(df.format(cantidadFinal));
 
         if (esDolar){
-            resultLiveData.setValue(cantidad + " Dolares = " + cf + " Euros.");
+            double cantidadFinal = Double.parseDouble(dol) * conversion;
+            double cf = Double.parseDouble(df.format(cantidadFinal));
+            resultLiveData.setValue(dol + " Dolares = " + cf + " Euros.");
         } else {
-            resultLiveData.setValue(cantidad + " Euros = " + cf + " Dolares.");
+            double cantidadFinal = Double.parseDouble(eur) / conversion;
+            double cf = Double.parseDouble(df.format(cantidadFinal));
+            resultLiveData.setValue(eur + " Euros = " + cf + " Dolares.");
         }
 
     }
+
+    // Recibe true si el RB de dolares está checkeado y setea falso para el Edit Text de euros
+    // y verdadero para el de dolares
+    public void setDolaresChecked(boolean isChecked) {
+        eurosET.setValue(!isChecked);
+        dolaresET.setValue(isChecked);
+    }
+    // Acá hace lo mismo con el de euros, si está checkeado, setea falso el de dolares
+    public void setEurosChecked(boolean isChecked) {
+        eurosET.setValue(isChecked);
+        dolaresET.setValue(!isChecked);
+    }
+
+
+
 }
 
